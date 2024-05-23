aebdbec4eaa9623d2be2388ddcb3423fc992a9f8
Bruno Silva Oliveira <86791361+Bruno514@users.noreply.github.com>
2024-05-23 08:11:09 -0300

    implement placeship function of gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,22 +1,62 @@
+import Ship from "./ship";
+
 export default class Gameboard {
   constructor() {
-    this.board = this.#generateBoard()
-    this.availableShipsSizes = [2, 2, 3, 3, 4, 5]
+    this.board = this.#generateBoard();
+    this.availableShipsSizes = [2, 2, 3, 3, 4, 5];
+  }
+
+  placeShip(startX, startY, endX, endY) {
+    [startX, startY, endX, endY].map((e) => {
+      if (e > 9 || e < 0) throw new Error("Coordinate is out of bounds");
+    });
+
+    const isHorizontal = startX === endX;
+    const staticRef = isHorizontal ? startX : startY;
+
+    const startRef = isHorizontal
+      ? Math.min(startY, endY)
+      : Math.min(startX, endX);
+    const endRef = isHorizontal
+      ? Math.max(startY, endY)
+      : Math.max(startX, endX);
+
+    const shipLength = endRef - startRef + 1;
+    if (!this.availableShipsSizes.includes(shipLength))
+      throw new Error("Unavailable ship");
+
+    const ship = new Ship(shipLength);
+    this.#removeShip(shipLength)
+
+    for (let ref = startRef; ref <= endRef; ref++) {
+      if (isHorizontal) {
+        if (!!this.board[ref][staticRef])
+          throw Error("This ship is overlapping another one");
+        this.board[ref][staticRef] = ship;
+      } else {
+        if (!!this.board[staticRef][ref])
+          throw Error("This ship is overlapping another one");
+        this.board[staticRef][ref] = ship;
+      }
+    }
   }
 
   #generateBoard() {
-    const board = []
+    const board = [];
     for (let row = 0; row < 10; row++) {
       board[row] = [];
       for (let col = 0; col < 10; col++) {
-        board[row][col] = 0
+        board[row][col] = 0;
       }
     }
 
-    return board
+    return board;
   }
 
-  placeShip(coordinateStart, coordinateEnd, length) {
-
+  #removeShip(size) {
+    if (this.availableShipsSizes.includes(size)) {
+      const index = this.availableShipsSizes.indexOf(size)
+      this.availableShipsSizes.splice(index, 1);
+    }
   }
 }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -21,89 +21,93 @@ test("check if gameboard is generated correctly", () => {
 
 test("check if ship is place correctly (horizontally following right)", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [2, 2], 3);
+  gameboard.placeShip(2, 0, 2, 2);
 
   for (let i = 0; i <= 2; i++) {
-    expect(gameboard.board[2][i]).not.toBe(0);
+    expect(gameboard.board[i][2]).not.toBe(0);
   }
 });
 
 test("check if ship is place correctly (vertically following downwards)", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [4, 0], 3);
+  gameboard.placeShip(2, 0, 4, 0);
 
-  for (let i = 0; i <= 2; i++) {
-    expect(gameboard.board[2][i]).not.toBe(0);
+  for (let i = 2; i <= 4; i++) {
+    expect(gameboard.board[0][i]).not.toBe(0);
   }
 });
 
 test("check if ship is place correctly (horizontally following left)", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 2], [2, 0], 3);
+  gameboard.placeShip(2, 2, 2, 0);
 
   for (let i = 0; i <= 2; i++) {
-    expect(gameboard.board[2][i]).not.toBe(0);
+    expect(gameboard.board[i][2]).not.toBe(0);
   }
 });
 
 test("check if ship is place correctly (vertically following upwards)", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [4, 0], 3);
+  gameboard.placeShip(2, 0, 4, 0);
 
-  for (let i = 0; i <= 2; i++) {
-    expect(gameboard.board[2][i]).not.toBe(0);
+  for (let i = 2; i <= 4; i++) {
+    expect(gameboard.board[0][i]).not.toBe(0);
   }
 });
 
-test("gameboard throws an error for an out of bounds X start coordinate", () => {
+test("gameboard throws an error for an out of bounds coordinate (1)", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.placeShip([0, 12], [0, 10])).toThrow(
-    "Start coordinate: X out of bounds"
-  );
+  expect(() => {
+    gameboard.placeShip(0, 12, 0, 10);
+  }).toThrow("Coordinate is out of bounds");
 });
 
-test("gameboard throws an error for an out of bounds Y start coordinate", () => {
+test("gameboard throws an error for an out of bounds coordinate (2)", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.placeShip([12, 0], [10, 0])).toThrow(
-    "Start coordinate: Y out of bounds"
-  );
+  expect(() => {
+    gameboard.placeShip(12, 0, 10, 0);
+  }).toThrow("Coordinate is out of bounds");
 });
 
-test("gameboard throws an error for an out of bounds X end coordinate", () => {
+test("gameboard throws an error for an out of bounds coordinate (3)", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.placeShip([0, 10], [0, 12])).toThrow(
-    "End coordinate: X out of bounds"
-  );
+  expect(() => {
+    gameboard.placeShip(0, 10, 0, 12);
+  }).toThrow("Coordinate is out of bounds");
 });
 
-test("gameboard throws an error for an out of bounds Y start coordinate", () => {
+test("gameboard throws an error for an out of bounds coordinate (4)", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.placeShip([10, 0], [12, 0])).toThrow(
-    "Start coordinate: Y out of bounds"
-  );
+  expect(() => {
+    gameboard.placeShip(10, 0, 12, 0);
+  }).toThrow("Coordinate is out of bounds");
+});
+
+test("tries to place ship on used coordinate", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(2, 0, 3, 0);
+
+  expect(() => {
+    gameboard.placeShip(3, 0, 4, 0);
+  }).toThrow("This ship is overlapping another one");
 });
 
 // Available ships at the beginning: size 2 x 2, size 3 x 2, size 4 x 1, size 5 x 1
 test("tries to place unavailable ship of length 4", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [5, 0]);
+  gameboard.placeShip(2, 0, 5, 0);
 
-  expect(gameboard.placeShip([2, 1], [5, 1])).toThrow("Unavailable ship");
+  expect(() => {
+    gameboard.placeShip(2, 1, 5, 1);
+  }).toThrow("Unavailable ship");
 });
 
 test("tries to place unavailable ship of length 2", () => {
   const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [3, 0]);
-  gameboard.placeShip([2, 1], [3, 1]);
+  gameboard.placeShip(2, 0, 3, 0);
+  gameboard.placeShip(2, 1, 3, 1);
 
-  expect(gameboard.placeShip([2, 2], [3, 2])).toThrow("Unavailable ship");
-});
-
-test("tries to place ship on used coordinate", () => {
-  const gameboard = new Gameboard();
-  gameboard.placeShip([2, 0], [3, 0]);
-
-  expect(gameboard.placeShip([3, 0], [4, 0])).toThrow(
-    "This ship is overlapping another one"
-  );
+  expect(() => {
+    gameboard.placeShip(2, 2, 3, 2);
+  }).toThrow("Unavailable ship");
 });
